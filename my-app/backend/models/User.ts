e838fb5a6f03d755db1a033dd6ff35a2092a4d90
Josh Mo <joshua.om.876@gmail.com>
2022-12-20 21:09:19 +0000

    + Change tsconfig.json to target esnext & use cjs + Add a seed file with an additional source file + add dotenv as a dev dependency + add environment.d.ts for adding env types + add db connection setup

--- my-app/backend/models/User.ts
+++ my-app/backend/models/User.ts
@@ -2,13 +2,13 @@ import mongoose from 'mongoose';
 const {Schema} = mongoose;
 
 const user_schema: mongoose.Schema = new Schema({
-    username: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     created_at: {type: Date, default: Date.now},
     phone_number: {type: String, required: false},
     refresh_token: {type: String, required: false},
     auth_token: {type: String, required: false}
 })
 
-module.exports = mongoose.model('user_schema', user_schema);
+export default mongoose.model('Users', user_schema);
