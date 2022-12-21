364477c4b12cc3e7013a18a5befa50ace2d2c6a0
Abrosss <nataroro5@gmail.com>
2022-12-21 22:31:25 +0600

    changed username, email in User model

--- my-app/backend/models/User.ts
+++ my-app/backend/models/User.ts
@@ -10,9 +10,9 @@ interface IUser {
     auth_token: string
   }
   const userSchema = new Schema<IUser>({
-    username: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     created_at: {type: Date, default: Date.now},
     phone_number: {type: String, required: false},
     refresh_token: {type: String, required: false},
